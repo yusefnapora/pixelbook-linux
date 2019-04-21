fb75ec556b6f647dedff152b9478ed33268f7f72
Yusef Napora <yusef@napora.org>
2019-04-21 07:26:14 -0400

    add script to run ansible on bare metal

---
+++ vagrant/ansible/run-ansible.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function ansible_installed {
+  dnf list installed | grep ansible > /dev/null
+}
+
+if [ ! ansible_installed ]; then
+	echo "ansible not found, installing"
+	sudo dnf install -y ansible
+fi
+
+echo "running ansible. this will take about an hour on the first run"
+
+exec ansible-playbook playbook.yml -i hosts -K
