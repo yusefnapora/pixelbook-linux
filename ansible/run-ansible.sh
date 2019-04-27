4ebae6743bd991f048cc157dd4bd1895fc72d8ff
Yusef Napora <yusef@napora.org>
2019-04-27 18:29:10 -0400

    update run script for ubuntu

--- ansible/run-ansible.sh
+++ ansible/run-ansible.sh
@@ -8,11 +8,21 @@ function is_installed {
   command -v $1 >/dev/null 2>&1
 }
 
+# make sure we're not running as root
+
+if [[ "$USER" == "root" ]];
+	echo "please don't run this script as the root user"
+	echo "in order to set things up properly,"
+        echo "you need to run with your main login account"
+	exit 1
+fi
+
 # install ansible if missing
 if ! is_installed ansible-playbook ; then
 	echo "ansible not found, installing"
-	sudo dnf install -y ansible
+	sudo apt-get install -y ansible
 fi
+
 
 echo "installing configuration with ansible. this may take a little while."
 
