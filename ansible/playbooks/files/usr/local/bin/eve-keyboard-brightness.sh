121c3a56103837d5460c93320cf9c91c13e23146
Yusef Napora <yusef@napora.org>
2019-04-27 08:57:48 -0400

    allow adjusting keyboard brightness by relative offsets

--- ansible/playbooks/files/usr/local/bin/eve-keyboard-brightness.sh
+++ ansible/playbooks/files/usr/local/bin/eve-keyboard-brightness.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 #
 
+DEVFILE=/sys/class/leds/chromeos\:\:kbd_backlight/brightness
+
+function get_current_brightness {
+    cat $DEVFILE
+}
+
+function adjust_relative {
+    local offset=$1
+    let "new = $(get_current_brightness) + $offset"
+    echo $new
+}
 
 if [[ "$1" =~ ^[0-9]+$ ]]; then
   brightness=$1
+elif [[ "$1" =~ ^[-+][0-9]+$ ]]; then
+    offset=$1
+    brightness=$(adjust_relative $offset)
 else
     echo "$0 <brightness>"
-    echo "	brightness must be an integer beteween 0 and 100"
+    echo "	brightness must be an integer beteween 0 and 100."
+    echo "  if brightness is prefixed with - or +, the number will be used as a relative adjustment to the current brightness."
     exit 1
 fi
 
-if [ $brightness -lt 0 -o $brightness -gt 100 ]; then
-  echo "error: brightness must be between 0 and 100"
-  exit 1
+if [ $brightness -lt 0 ]; then
+    brightness=0
 fi
 
-DEVFILE=/sys/class/leds/chromeos\:\:kbd_backlight/brightness
+if [ $brightness -gt 100 ]; then
+    brightness=100
+fi
 
 echo $brightness > $DEVFILE
+
+echo "set backlight brightness to $(get_current_brightness)"
